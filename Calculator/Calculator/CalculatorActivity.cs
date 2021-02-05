5a64e2ceb2f8afafe7455486659f3bb8f65a68bd
Martin Makejev <martin.makejev@tptlive.ee>
2021-02-06 01:08:07 +0200

    Kalkulaatori loomine

---
+++ Calculator/Calculator/CalculatorActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    [Activity(Label = "CalculatorActivity")]
+    public class CalculatorActivity : Activity
+    {
+        EditText firstEditText;
+        EditText secondEditText;
+        TextView answerText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.calculator_layout);
+
+            firstEditText = FindViewById<EditText>(Resource.Id.firstNumberEditText);
+            secondEditText = FindViewById<EditText>(Resource.Id.secondNumberEditText);
+            var addButton = FindViewById<Button>(Resource.Id.addButton);
+            var subButton = FindViewById<Button>(Resource.Id.subButton);
+            var mulButton = FindViewById<Button>(Resource.Id.mulButton);
+            var divButton = FindViewById<Button>(Resource.Id.divButton);
+            answerText = FindViewById<TextView>(Resource.Id.answerTextView);
+
+            addButton.Click += AddButton_Click;
+            subButton.Click += ButtonSub;
+            mulButton.Click += ButtonMul;
+            divButton.Click += ButtonDiv;
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            var answer = int.Parse(firstEditText.Text) + int.Parse(secondEditText.Text);
+            answerText.Text = answer.ToString();
+        }
+
+        void ButtonSub(object sender, EventArgs e)
+        {
+            var answer = int.Parse(firstEditText.Text) - int.Parse(secondEditText.Text);
+            answerText.Text = answer.ToString();
+        }
+        void ButtonMul(object sender, EventArgs e)
+        {
+            var answer = int.Parse(firstEditText.Text) * int.Parse(secondEditText.Text);
+            answerText.Text = answer.ToString();
+        }
+        void ButtonDiv(object sender, EventArgs e)
+        {
+            var answer = int.Parse(firstEditText.Text) / int.Parse(secondEditText.Text);
+            answerText.Text = answer.ToString();
+        }
+    }
+}
--- Calculator/Calculator/MainActivity.cs
+++ Calculator/Calculator/MainActivity.cs
@@ -3,8 +3,9 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 
-namespace App1
+namespace Calculator
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
@@ -15,18 +16,16 @@ namespace App1
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-
-            var textView = FindViewById<TextView>(Resource.Id.textView1);
             var button = FindViewById<Button>(Resource.Id.button1);
-            var counter = 0;
-
-            button.Click += delegate
-            {
-                counter++;
-                textView.Text = "You clicked the button";
-                button.Text = "Count:" + counter;
-            };
+            button.Click += Button_Click;
         }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(CalculatorActivity));
+            StartActivity(intent);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
